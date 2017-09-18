dd89581e269e28d552f2b0017788a58c8244867c
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-09-18 08:02:48 +0200

    ajout des avatar random  attribué a chaque utilisateur a leur connection

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,33 @@
 $(function(){
 
+	
+	// var avatar = '<img class="avatar" src="https://api.adorable.io/avatars/40/'+random+'"/>';
+	// console.log(avatar);
+	
+
 	var socket = io();
 
 	$('#chat').submit(function(e){
 		e.preventDefault();
-		socket.emit('chat message', $('#chatMessage').val());
-		$('#chatMessage').val('');
+		// fonction qui gere si les inputs des messages et vide ou pas  
+		if ($('#chatMessage').val()!==("")) {
+			socket.emit('chat message', $('#chatMessage').val());
+			$('#chatMessage').val('');
+		} else {
+			$('#erreurMessage').html('<p class="text-danger">Veuillez entrer un message</p>');
+		}
+		
 	});
 
 	socket.on('message', function(msg){
-		console.log(msg);
+		// fonction qui gere les envois de message dans le chat
+		// var imageUser = avatar+ " "+;
+		
 		$('#message').prepend('<p><strong class="text-primary">'+msg.user+ ' : </strong><span class="text-success">' + msg.message + '</span></p>');
 	});
 
 	socket.on('service-message', function(user){
-
+		// fonction qui gere quand une personne se connecte sur le chat
 		$('#message').prepend('<strong><div class="text-success">'+user+' est en ligne !</div></strong>');
 	});
 
@@ -22,10 +35,17 @@ $(function(){
 
 		e.preventDefault();
 		var username = $('#pseudo');
-		socket.emit('login', username.val());
-		username.val('');
-		$('#chat').removeClass('hidden');
-		$('#co').addClass('hidden');
+
+		if ($('#pseudo').val()!==('')) {
+			socket.emit('login', username.val());
+			username.val('');
+			$('#chat').removeClass('hidden');
+			$('#co').addClass('hidden');
+		} else {
+			$('#erreurPseudo').html('<p class="text-danger">Veuillez entrez un pseudo</p>');
+		}
+		
+		
 	});
 
 });
--- server.js
+++ server.js
@@ -7,24 +7,29 @@ var io = socket(server);
 var ent = require('ent');
 
 
-
-
 app.use(express.static('public'));
 
 //Action sur la connection 
 io.on('connection', function(socket){
 	var userLog;  
 	socket.on('login', function(user){
+		
 		userLog = ent.encode(user);
 		socket.broadcast.emit('service-message', userLog);
 	});
 
 	socket.on('chat message', function(msg){
 		var message = ent.encode(msg);
-		io.emit('message', {message: message, user: userLog});
+		var min = 1;
+		var max = 100000;
+		var random = Math.floor(Math.random()* (max - min)+ min);
+		var imageUser = '<img class="avatar" src="https://api.adorable.io/avatars/40/'+random+'"/>'+ " ";
+		
+		io.emit('message', {message: message, user: imageUser+userLog});
 	});	
-
+	console.log("User is connected");
+	
 	socket.on('disconnect', function(){
-		console.log("User is disconnected ! ");
+		console.log("User is disconnected !");
 	});
 });
